fad791ce6aa1f905352c36d1578de0d617eaa39a
justin <jv.datamail@gmail.com>
2022-03-04 14:36:10 +0100

    auto formatted

--- bubble/api.py
+++ bubble/api.py
@@ -1,46 +1,55 @@
-import requests
 import urllib
+
 import pandas as pd
-from bubble.utils.read_transformer import create_base_url_api, check_base_params, add_constraints
+import requests
+
+from bubble.utils.read_transformer import (
+    add_constraints,
+    check_base_params,
+    create_base_url_api,
+)
+
 
 class BubbleApi(object):
-
     def __init__(self, params, website, env, **kwargs):
 
-        
-        self._env_version = kwargs.get('env_version', None)
+        self._env_version = kwargs.get("env_version", None)
         self._base_params = check_base_params(params)
         self._env = env
         self._website = website
-        self._base_url_api = create_base_url_api(website, 
-                                                 env, 
-                                                 dev_version=self._env_version)
+        self._base_url_api = create_base_url_api(
+            website, env, dev_version=self._env_version
+        )
 
     def get(self, bubble_object, **kwargs):
-        
+
         constraints_params = kwargs.get("constraints_params", None)
-    
+
         cursor = 0
         remaining = 100
         concatenate_res = list()
         read_params = self._base_params
-        
+
         if constraints_params is not None:
             read_params = add_constraints(read_params, constraints_params)
-        
+
         session = requests.Session()
-                       
+
         while remaining > 0:
 
             read_params["cursor"] = cursor
 
-            url = self._base_url_api + bubble_object + \
-                '?' + urllib.parse.urlencode(read_params)
+            url = (
+                self._base_url_api
+                + bubble_object
+                + "?"
+                + urllib.parse.urlencode(read_params)
+            )
             response = session.get(url)
-            
-            concatenate_res.append(response.json()['response']["results"])
-            remaining = response.json()['response']['remaining']
-            count = response.json()['response']['count']
+
+            concatenate_res.append(response.json()["response"]["results"])
+            remaining = response.json()["response"]["remaining"]
+            count = response.json()["response"]["count"]
 
             cursor += count
 
@@ -54,10 +63,6 @@ class BubbleApi(object):
 
         for i in range(0, len(json_list)):
 
-            concat_df = pd.concat([
-                concat_df,
-                pd.json_normalize(json_list[i])
-            ], axis=0)
+            concat_df = pd.concat([concat_df, pd.json_normalize(json_list[i])], axis=0)
 
         return concat_df
-
